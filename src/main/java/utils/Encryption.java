1f25c22b2abedd4ef4d3acfbd2240e613682fa96
mtc97 <magnustilsted@gmail.com>
2018-12-21 10:01:06 +0100

    sidste rettelser + encryp=true

--- src/main/java/utils/Encryption.java
+++ src/main/java/utils/Encryption.java
@@ -15,6 +15,9 @@ public final class Encryption {
       StringBuilder thisIsEncrypted = new StringBuilder();
 
       // TODO: This is where the magic of XOR is happening. Are you able to explain what is going on?
+      // For every value that is going to be encrypted this for-loop iterates through what is typed in and
+      // adds the binary value of EncrypKey which is declared as key in line 12. We use a stringbuid to create an
+      // instance "thisIsEncrypted".
       for (int i = 0; i < rawString.length(); i++) {
         thisIsEncrypted.append((char) (rawString.charAt(i) ^ key[i % key.length]));
       }
